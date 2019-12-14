f35287e570caa89d66a5e703fc64cbe91ab64a76
Sabet <is00320@heron166.eps.surrey.ac.uk>
2019-12-14 16:15:43 +0000

    Database schema and crucial seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+result = Result.where(name: 'init').first
+result.delete if result
+
+initResult = Result.create([id: 1, name: 'init'])
